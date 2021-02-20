b158d6d0fb757fdbe86931e3ae3f47a218fe5139
Dilara <dilarauniversity@gmail.com>
2021-02-19 16:03:14 -0800

    Look on this awesome CHANGE!!!

--- src/group22/FirstClass.java
+++ src/group22/FirstClass.java
@@ -7,9 +7,20 @@ public class FirstClass{
         String word = "Sergii";
         //             012345
 
-        for(int i=word.length()-1; i>=0;i-- ){
+       /* for(int i=word.length()-1; i>=0;i-- ){
             System.out.print(word.charAt(i));
         }
+
+        */
+
+        String result = "";
+        for(int i=word.length()-1; i>=0;i-- ){
+            result += word.charAt(i);
+        }
+        System.out.println(result);
+
+
+
     }
 }
 
